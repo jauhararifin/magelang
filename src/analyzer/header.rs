66276b1f068fb946610353676cf0f1eb11b3b701
Jauhar Arifin <jauhararifin10@gmail.com>
2022-05-05 08:04:34 +0800

    refactor header

--- src/analyzer/header.rs
+++ src/analyzer/header.rs
@@ -2,7 +2,7 @@ use std::rc::Rc;
 
 use crate::{
     analyzer::types::TypeHelper,
-    ast::RootNode,
+    ast::{FnDeclNode, RootNode},
     errors::Error,
     semantic::{FnHeader, Header},
 };
@@ -30,25 +30,41 @@ impl IHeaderCompiler for HeaderCompiler {
 
         let fn_decls = root.declarations.iter().filter_map(|decl| decl.try_unwrap_func());
         for func in fn_decls {
-            let name = func.name.clone_value();
-
-            if expr_helper.find_symbol(&name).is_some() {
-                return Err(Error::RedeclaredSymbol);
-            }
-
-            let typ = type_helper.get_fn(&func.header);
-
-            let fn_type = typ.unwrap_func();
-            functions.push(FnHeader {
-                name: name.clone(),
-                native: fn_type.native,
-                fn_type: fn_type.clone(),
-                typ: typ.clone(),
-            });
-
-            expr_helper.add_symbol(Rc::new(Symbol { name, typ }));
+            let func = self.compile_fn_header(&mut expr_helper, &type_helper, func)?;
+            functions.push(func);
         }
 
         Ok(Header { functions })
+    }
+}
+
+impl HeaderCompiler {
+    fn compile_fn_header(
+        &self,
+        expr_helper: &mut ExprHelper,
+        type_helper: &TypeHelper,
+        func: &FnDeclNode,
+    ) -> Result<FnHeader, Error> {
+        let name = func.name.clone_value();
+
+        if expr_helper.find_symbol(&name).is_some() {
+            return Err(Error::RedeclaredSymbol);
+        }
+
+        let typ = type_helper.get_fn(&func.header);
+
+        let fn_type = typ.unwrap_func();
+
+        expr_helper.add_symbol(Rc::new(Symbol {
+            name: name.clone(),
+            typ: typ.clone(),
+        }));
+
+        Ok(FnHeader {
+            name,
+            native: fn_type.native,
+            fn_type: fn_type.clone(),
+            typ,
+        })
     }
 }
