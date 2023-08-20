9a1b4d04476726b85b219061f31005da21819833
Jauhar Arifin <jauhararifin10@gmail.com>
2023-08-20 20:11:05 +0700

    group the interners together into a context struct

--- magelang_analyzer/src/analyze.rs
+++ magelang_analyzer/src/analyze.rs
@@ -34,8 +34,24 @@ pub fn analyze(
         main_package,
     );
 
-    let object_nodes = build_object_nodes(&file_manager, error_manager, &symbols, package_asts);
-    build_symbol_table(&file_manager, error_manager, &symbols, &types, object_nodes);
+    let ctx = Context {
+        files: file_manager,
+        errors: error_manager,
+        symbols: &symbols,
+        types: &types,
+        typeargs: &typeargs,
+    };
+
+    let object_nodes = build_object_nodes(&ctx, package_asts);
+    let symbol_tables = build_symbol_table(&ctx, object_nodes);
+}
+
+struct Context<'a, E> {
+    files: &'a FileManager,
+    errors: &'a E,
+    symbols: &'a SymbolInterner,
+    types: &'a TypeInterner,
+    typeargs: &'a TypeArgsInterner,
 }
 
 fn get_all_package_asts(
@@ -83,10 +99,8 @@ fn get_all_package_asts(
     package_asts
 }
 
-fn build_object_nodes(
-    files: &FileManager,
-    errors: &impl ErrorReporter,
-    symbols: &SymbolInterner,
+fn build_object_nodes<'ctx, E: ErrorReporter>(
+    ctx: &Context<'ctx, E>,
     package_asts: IndexMap<SymbolId, PackageNode>,
 ) -> IndexMap<DefId, ItemNode> {
     let mut object_nodes = IndexMap::<DefId, ItemNode>::default();
@@ -94,7 +108,7 @@ fn build_object_nodes(
 
     for (package_name, package_ast) in package_asts {
         for item in package_ast.items {
-            let object_name = symbols.define(item.name());
+            let object_name = ctx.symbols.define(item.name());
             let object_id = DefId {
                 package: package_name,
                 name: object_name,
@@ -102,8 +116,9 @@ fn build_object_nodes(
 
             let pos = item.pos();
             if let Some(declared_at) = object_pos.get(&object_id) {
-                let declared_at = files.location(*declared_at);
-                errors.redeclared_symbol(pos, declared_at, &symbols.get(object_name));
+                let declared_at = ctx.files.location(*declared_at);
+                ctx.errors
+                    .redeclared_symbol(pos, declared_at, &ctx.symbols.get(object_name));
                 continue;
             }
 
@@ -171,24 +186,17 @@ struct GenericFuncObject {
     annotations: Rc<[Annotation]>,
 }
 
-fn build_symbol_table(
-    files: &FileManager,
-    errors: &impl ErrorReporter,
-    symbols: &SymbolInterner,
-    types: &TypeInterner,
+fn build_symbol_table<'ctx, E: ErrorReporter>(
+    ctx: &Context<'ctx, E>,
     object_nodes: IndexMap<DefId, ItemNode>,
 ) -> IndexMap<DefId, Object> {
     let mut symbol_table = IndexMap::<DefId, Object>::default();
     for (def_id, item_node) in object_nodes {
         let object = match item_node {
-            ItemNode::Import(node) => init_import_object(errors, symbols, node),
-            ItemNode::Struct(node) => Some(init_struct_objects(
-                files, errors, symbols, types, def_id, node,
-            )),
+            ItemNode::Import(node) => init_import_object(ctx, node),
+            ItemNode::Struct(node) => Some(init_struct_objects(ctx, def_id, node)),
             ItemNode::Global(global_node) => Some(init_global_object(def_id, global_node)),
-            ItemNode::Function(func_node) => Some(init_function_object(
-                files, errors, symbols, def_id, func_node,
-            )),
+            ItemNode::Function(func_node) => Some(init_function_object(ctx, def_id, func_node)),
         };
         let Some(object) = object else { continue };
 
@@ -197,34 +205,30 @@ fn build_symbol_table(
     symbol_table
 }
 
-fn init_import_object(
-    errors: &impl ErrorReporter,
-    symbols: &SymbolInterner,
+fn init_import_object<'ctx, E: ErrorReporter>(
+    ctx: &Context<'ctx, E>,
     import_node: ImportNode,
 ) -> Option<Object> {
     let import_path = import_node.path.value.as_str();
     let Some(package_path) = value_from_string_lit(import_path) else {
-        errors.invalid_utf8_package(import_node.path.pos);
+        ctx.errors.invalid_utf8_package(import_node.path.pos);
         return None;
     };
     let package_path = match String::from_utf8(package_path) {
         Ok(v) => v,
         Err(..) => {
-            errors.invalid_utf8_package(import_node.path.pos);
+            ctx.errors.invalid_utf8_package(import_node.path.pos);
             return None;
         }
     };
-    let package_name = symbols.define(package_path.as_str());
+    let package_name = ctx.symbols.define(package_path.as_str());
     Some(Object::Import(ImportObject {
         package: package_name,
     }))
 }
 
-fn init_struct_objects(
-    files: &FileManager,
-    errors: &impl ErrorReporter,
-    symbols: &SymbolInterner,
-    types: &TypeInterner,
+fn init_struct_objects<'ctx, E: ErrorReporter>(
+    ctx: &Context<'ctx, E>,
     def_id: DefId,
     struct_node: StructNode,
 ) -> Object {
@@ -234,11 +238,10 @@ fn init_struct_objects(
             node: struct_node,
             body: OnceCell::default(),
         });
-        let type_id = types.define(ty);
+        let type_id = ctx.types.define(ty);
         Object::Type(type_id)
     } else {
-        let type_params =
-            get_typeparams_from_node(files, errors, symbols, &struct_node.type_params);
+        let type_params = get_typeparams_from_node(ctx, &struct_node.type_params);
         Object::GenericStruct(GenericStructObject {
             def_id,
             type_params,
@@ -248,21 +251,19 @@ fn init_struct_objects(
     }
 }
 
-fn get_typeparams_from_node(
-    files: &FileManager,
-    errors: &impl ErrorReporter,
-    symbols: &SymbolInterner,
+fn get_typeparams_from_node<'ctx, E: ErrorReporter>(
+    ctx: &Context<'ctx, E>,
     node: &[TypeParameterNode],
 ) -> IndexSet<SymbolId> {
     let mut type_param_pos = HashMap::<SymbolId, Pos>::default();
     for type_param_node in node {
         let type_name = type_param_node.name.value.as_str();
-        let type_param_name = symbols.define(type_name);
+        let type_param_name = ctx.symbols.define(type_name);
         let pos = type_param_node.name.pos;
         if let Some(defined_at) = type_param_pos.get(&type_param_name) {
-            errors.redeclared_symbol(
+            ctx.errors.redeclared_symbol(
                 pos,
-                files.location(*defined_at),
+                ctx.files.location(*defined_at),
                 &type_param_node.name.value,
             );
         } else {
@@ -281,18 +282,16 @@ fn init_global_object(def_id: DefId, global_node: GlobalNode) -> Object {
     })
 }
 
-fn init_function_object(
-    files: &FileManager,
-    errors: &impl ErrorReporter,
-    symbols: &SymbolInterner,
+fn init_function_object<'ctx, E: ErrorReporter>(
+    ctx: &Context<'ctx, E>,
     def_id: DefId,
     func_node: FunctionNode,
 ) -> Object {
     if func_node.body.is_none() {
-        return init_native_function_object(files, errors, symbols, def_id, func_node.signature);
+        return init_native_function_object(ctx, def_id, func_node.signature);
     }
 
-    let annotations = build_annotations_from_node(errors, &func_node.signature).into();
+    let annotations = build_annotations_from_node(ctx, &func_node.signature).into();
     if func_node.signature.type_params.is_empty() {
         Object::Func(FuncObject {
             def_id,
@@ -302,8 +301,7 @@ fn init_function_object(
             annotations,
         })
     } else {
-        let type_params =
-            get_typeparams_from_node(files, errors, symbols, &func_node.signature.type_params);
+        let type_params = get_typeparams_from_node(ctx, &func_node.signature.type_params);
         Object::GenericFunc(GenericFuncObject {
             def_id,
             signature: func_node.signature,
@@ -321,8 +319,8 @@ pub struct Annotation {
     pub arguments: Vec<String>,
 }
 
-fn build_annotations_from_node(
-    errors: &impl ErrorReporter,
+fn build_annotations_from_node<'ctx, E: ErrorReporter>(
+    ctx: &Context<'ctx, E>,
     signature: &SignatureNode,
 ) -> Vec<Annotation> {
     let mut annotations = Vec::default();
@@ -331,13 +329,13 @@ fn build_annotations_from_node(
         let mut valid = true;
         for arg in &annotation_node.arguments {
             let Some(arg_value) = value_from_string_lit(&arg.value) else {
-                errors.invalid_utf8_string(arg.pos);
+                ctx.errors.invalid_utf8_string(arg.pos);
                 valid = false;
                 continue;
             };
 
             let Ok(arg_value) = String::from_utf8(arg_value) else {
-                errors.invalid_utf8_string(arg.pos);
+                ctx.errors.invalid_utf8_string(arg.pos);
                 valid = false;
                 continue;
             };
@@ -354,14 +352,12 @@ fn build_annotations_from_node(
     annotations
 }
 
-fn init_native_function_object(
-    files: &FileManager,
-    errors: &impl ErrorReporter,
-    symbols: &SymbolInterner,
+fn init_native_function_object<'ctx, E: ErrorReporter>(
+    ctx: &Context<'ctx, E>,
     def_id: DefId,
     signature: SignatureNode,
 ) -> Object {
-    let annotations = build_annotations_from_node(errors, &signature).into();
+    let annotations = build_annotations_from_node(ctx, &signature).into();
     if signature.type_params.is_empty() {
         Object::Func(FuncObject {
             def_id,
@@ -371,7 +367,7 @@ fn init_native_function_object(
             annotations,
         })
     } else {
-        let type_params = get_typeparams_from_node(files, errors, symbols, &signature.type_params);
+        let type_params = get_typeparams_from_node(ctx, &signature.type_params);
         Object::GenericFunc(GenericFuncObject {
             def_id,
             signature,
